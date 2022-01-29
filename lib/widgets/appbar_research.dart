5f3b59d2d971b990ea9413e726c88c7442e55994
Limin0109 <87058798+Limin0109@users.noreply.github.com>
2022-01-30 01:04:17 +0800

    Delete lib directory

--- lib/widgets/appbar_research.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-AppBar appbarResearch(BuildContext context) {
-  return AppBar(
-    title: Text('Research Centre'),
-    backgroundColor: Colors.lightBlue[100],
-    elevation: 0,
-  );
-}
